60d00d7cb57eb0f076e961ad93c1d35799f72326
Moundier <garamoundier.developer@protonmail.com>
2024-01-22 17:50:48 -0300

    feat: tab size!

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -7,14 +7,10 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
     }
 }
